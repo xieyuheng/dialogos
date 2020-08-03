f2f5c92d221e40f67f4d1978fd81c794ff9a9e7a
Xie Yuheng <xyheme@gmail.com>
2020-08-03 11:37:16 +0800

    up

--- packages/little/src/cli/cli-hub.ts
+++ packages/little/src/cli/cli-hub.ts
@@ -1,8 +1,9 @@
 import * as hub from "../hub"
-import portfinder from "portfinder"
+import get_port from "get-port"
 
 export async function run(file: string, opts: any): Promise<void> {
-  const port = opts.port || (await portfinder.getPortPromise({ port: 3000 }))
-  const wsport = await portfinder.getPortPromise({ port: port + 100 })
+  const port = opts.port || (await get_port({ port: 3000 }))
+  const wsport = await get_port({ port })
+
   hub.server.run(file, port, wsport)
 }
