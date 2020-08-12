95056def3bd27636b4afd445f04a46e507ca77c4
Xie Yuheng <xyheme@gmail.com>
2020-08-12 23:32:14 +0800

    up

--- src/routes/book/[name].svelte
+++ src/routes/book/[name].svelte
@@ -1,6 +1,7 @@
 <script context="module">
   export async function preload({ params }) {
     const { name } = params
+
     const res = await this.fetch(`data/${name}`)
     const frames = await res.json()
 
