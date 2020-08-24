cc06c65293540c7a2e3b9a223471205c318f0d64
Xie Yuheng <xyheme@gmail.com>
2020-08-24 11:32:31 +0800

    up

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,32 +2,36 @@
   <title>ΔΙΆΛΟΓΟΣ</title>
 </svelte:head>
 
-<h1>ΔΙΆΛΟΓΟΣ</h1>
-
-<p>
-  An online dialectic teaching system,
-  <br />
-  inspired by Socrates, Plato, George Pólya and Dan Friedman.
-</p>
-
-<img src="cute-dialog-64px.png" alt="cute-dialog" width="64" height="64" />
-<img
-  src="cute-ask-question-64px.png"
-  alt="cute-ask-question"
-  width="64"
-  height="64" />
-<img src="cute-ok-64px.png" alt="cute-ok" width="64" height="64" />
-
-<ul>
-  List of dialogs:
-  <li>
-    <a href="book/the-little-typer">The Little Typer</a>
-  </li>
-</ul>
+<main>
+  <h1>ΔΙΆΛΟΓΟΣ</h1>
+  <p>
+    An online dialectic teaching system,
+    <br />
+    inspired by Socrates, Plato, George Pólya and Dan Friedman.
+  </p>
+  <img src="cute-dialog-64px.png" alt="cute-dialog" width="64" height="64" />
+  <img
+    src="cute-ask-question-64px.png"
+    alt="cute-ask-question"
+    width="64"
+    height="64" />
+  <img src="cute-ok-64px.png" alt="cute-ok" width="64" height="64" />
+  <ul>
+    List of dialogs:
+    <li>
+      <a href="book/the-little-typer">The Little Typer</a>
+    </li>
+  </ul>
+</main>
 
 <style>
-  * {
+  main {
+    height: 100vh;
     background: #f9dbd2;
     color: #333333;
+  }
+
+  li {
+    list-style-type: circle;
   }
 </style>
