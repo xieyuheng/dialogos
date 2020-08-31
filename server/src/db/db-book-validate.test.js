ac3b2fc902d95cd1e7998be8bb36d5a58e9fe49f
Xie Yuheng <xyheme@gmail.com>
2020-08-31 12:28:10 +0800

    example inline_schema

--- server/src/db/db-book-validate.test.js
+++ server/src/db/db-book-validate.test.js
@@ -4,10 +4,50 @@ import * as ut from "../ut"
 
 const ajv = new Ajv()
 
-// {
-//   const validate = ajv.compile(schema)
-//   const valid = validate(data)
-//   if (!valid) console.log(validate.errors)
-// }
+{
+  const data = {
+    first_name: "George",
+    last_name: "Washington",
+    birthday: "1732-02-22",
+    address: {
+      street_address: "3200 Mount Vernon Memorial Highway",
+      city: "Mount Vernon",
+      state: "Virginia",
+      country: "United States",
+    },
+  }
 
-// console.log var valid = ajv.validate(schema, data);
+  const inline_schema = {
+    first_name: "string",
+    last_name: "string",
+    birthday: "string|date",
+    address: {
+      street_address: "string",
+      city: "string",
+      state: "string",
+      country: "string",
+    },
+  }
+
+  const schema = {
+    type: "object",
+    properties: {
+      first_name: { type: "string" },
+      last_name: { type: "string" },
+      birthday: { type: "string", format: "date" },
+      address: {
+        type: "object",
+        properties: {
+          street_address: { type: "string" },
+          city: { type: "string" },
+          state: { type: "string" },
+          country: { type: "string" },
+        },
+      },
+    },
+  }
+
+  const validate = ajv.compile(schema)
+  const valid = validate(data)
+  if (!valid) console.log(validate.errors)
+}
