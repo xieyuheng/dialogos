d111adea905af146f588e21a6b7750521602bb8f
Xie Yuheng <xyheme@gmail.com>
2020-09-13 14:09:01 +0800

    up

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -3,5 +3,5 @@ const { __PRODUCTION__ } = process.env
 export const web = {
   url: __PRODUCTION__
     ? "http://vmi436272.contaboserver.net"
-    : "http://localhost:5000",
+    : "http://0.0.0.0:5000",
 }
--- web/src/config/index.js
+++ web/src/config/index.js
@@ -1,5 +1,5 @@
 export const server = {
   url: __PRODUCTION__
     ? "http://vmi436272.contaboserver.net:3000"
-    : "http://localhost:3000",
+    : "http://0.0.0.0:3000",
 }
